70944d65b2d1123cd029e2f7b084adbd1dcf652b
Dmitry Pupinin <dlnsk@mail.ru>
2025-12-02 16:48:46 +0700

    Base structure

---
+++ src/UQFIServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dlnsk\UQFI;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class UQFIServiceProvider extends ServiceProvider
+{
+    /**
+     * This will be used to register config & view in
+     * package namespace.
+     */
+    protected $packageName = 'uqfi';
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
