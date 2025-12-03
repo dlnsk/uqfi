6d2d5754686a4c61fe4782a9d467cea061a4e1e1
Dmitry Pupinin <dlnsk@mail.ru>
2025-12-03 11:28:19 +0700

    Refactor files structure

--- src/UQFIServiceProvider.php
+++ src/UQFIServiceProvider.php
@@ -21,8 +21,8 @@ class UQFIServiceProvider extends ServiceProvider
     public function register()
     {
         define("MOODLE_INTERNAL", "dummy");
-        require_once(__DIR__ . '/../qformats/functions.php');
-        require_once(__DIR__ . '/../qformats/xmlize.php');
+        require_once(__DIR__ . '/../lib/functions.php');
+        require_once(__DIR__ . '/../lib/xmlize.php');
     }
 
     /**
